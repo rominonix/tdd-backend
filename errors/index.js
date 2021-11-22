0a33e10c5814bf9996d77eb7ee6674961e467480
Romina Rebolledo <rominarebolledo@gmail.com>
2021-11-22 14:21:48 +0100

    update error.js

--- errors/index.js
+++ errors/index.js
@@ -4,7 +4,7 @@ class InvalidBody extends productError{
     constructor(fields){
         super()
         this.fields=fields
-        this.message=`Invalid body, required field:${this.fields.join(",")}``
+        this.message=`Invalid body, required field:${this.fields.join(",")}`
         this.errorCode=400
     }
 }
