f1c3bda016e2c5133bc88e2a6e69a05bcb4f8788
nilufar1026 <nilux1026@gmail.com>
2021-11-25 15:39:33 +0100

    test for update cartItem and delete cartItem

--- test/cart.test.js
+++ test/cart.test.js
@@ -1,61 +1,48 @@
 const request = require("supertest");
 const app = require("../app");
 
-
-
 /**
 * Testing delete cartItem endpoint
 */
-describe('DELETE /api/carts/:userLogin/:itemId',function(){
-  it('should respond 200 deleted',function(done){
-    request(app)
-    .delete('/api/carts/ce6ab499-60fe-43b4-9a1c-e4590edc0540/f7bd1b82-e3cf-4252-b57a-3dbe0dccda13')
-    .expect(200)
-    .expect('Content-Type', /json/)
-    .expect((res) => {
-      expect(res.body.message).toEqual('cart item has deleted');
-    })
-    .end((err)=>{
-      if(err) return done(err)
-      done();
-    })
-  });
-})
+// describe('DELETE /api/carts/:userLogin/:itemId',function(){
+//   it('should respond 200 deleted',function(done){
+//     request(app)
+//     .delete('/api/carts/ce6ab499-60fe-43b4-9a1c-e4590edc0540/f7bd1b82-e3cf-4252-b57a-3dbe0dccda13')
+//     .expect(200)
+//     .expect('Content-Type', /json/)
+//     .expect((res) => {
+//       expect(res.body.message).toEqual('cart item has deleted');
+//     })
+//     .end((err)=>{
+//       if(err) return done(err)
+//       done();
+//     })
+//   });
+// })
 
 /**
 * Testing update cartItem endpoint
 */
-describe('update /api/carts/:userLogin/:itemId',function(){
-    it('should respond 200 updated',function(done){
-      request(app)
-      .delete('/api/carts/ce6ab499-60fe-43b4-9a1c-e4590edc0540/f7bd1b82-e3cf-4252-b57a-3dbe0dccda13')
-      .expect(200)
-      .expect('Content-Type', /json/)
-      .expect((res) => {
-        expect(res.body.message).toEqual('cart item has deleted');
-      })
-      .end((err)=>{
-        if(err) return done(err)
-        done();
-      })
+describe('update /api/carts/:userLogin/:itemId', function () {
+    let data = { "amount": 3 }
+    it('respond with 200 update', function (done) {
+        request(app)
+            .put('/api/carts/0b091260-10fe-4ac6-b122-e58a398587d9/417c5d33-c82d-4515-8879-b5e8ce7695f4')
+            .send(data)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end((err) => {
+                if (err) return done(err);
+                done();
+            });
     });
-  })
+})
 
-  it('should update user', async () => {
-    delete dbUsers.branStark.password;
-    const id = (await User.findOne(dbUsers.branStark))._id;
-    const { name } = user;
- 
-    return request(app)
-     .patch('/api/carts/0b091260-10fe-4ac6-b122-e58a398587d9/417c5d33-c82d-4515-8879-b5e8ce7695f4')
-     .set('Authorization', `Bearer ${adminAccessToken}`)
-     .send({ name })
-     .expect(httpStatus.OK)
-     .then((res) => {
-      expect(res.body.name).to.be.equal(name);
-      expect(res.body.email).to.be.equal(dbUsers.branStark.email);
-     });
-   });
+
+
+
+
 
 
 
