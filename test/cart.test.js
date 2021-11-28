4a935bc1f4b78ead1e1da8882a4d6da6fe1adbc5
nilufar1026 <nilux1026@gmail.com>
2021-11-28 10:29:42 +0100

    add update and delete endpoints tests

--- test/cart.test.js
+++ test/cart.test.js
@@ -1,30 +1,10 @@
 const request = require("supertest");
 const app = require("../app");
 
-/**
-* Testing delete cartItem endpoint
-*/
-// describe('DELETE /api/carts/:userLogin/:itemId',function(){
-//   it('should respond 200 deleted',function(done){
-//     request(app)
-//     .delete('/api/carts/ce6ab499-60fe-43b4-9a1c-e4590edc0540/f7bd1b82-e3cf-4252-b57a-3dbe0dccda13')
-//     .expect(200)
-//     .expect('Content-Type', /json/)
-//     .expect((res) => {
-//       expect(res.body.message).toEqual('cart item has deleted');
-//     })
-//     .end((err)=>{
-//       if(err) return done(err)
-//       done();
-//     })
-//   });
-// })
-
-/**
-* Testing update cartItem endpoint
-*/
+/* Testing update cartItem endpoint*/
 describe('update /api/carts/:userLogin/:itemId', function () {
     let data = { "amount": 3 }
+    let invalidData = { "amount": "hello" }
     it('respond with 200 update', function (done) {
         request(app)
             .put('/api/carts/0b091260-10fe-4ac6-b122-e58a398587d9/417c5d33-c82d-4515-8879-b5e8ce7695f4')
@@ -37,66 +17,44 @@ describe('update /api/carts/:userLogin/:itemId', function () {
                 done();
             });
     });
+    it('should return error if amount is not number', async ()=> {
+        const response = await request(app)
+        .put('/api/0b091260-10fe-4ac6-b122-e58a398587d9/417c5d33-c82d-4515-8879-b5e8ce7695f4')
+        .send(invalidData)
+        expect(response.statusCode).toBe(404)
+        expect(response.body.message).toBe(undefined)
+    });
+    it('should return a error message by putting data to a non-existing item id ', async () => {
+        const response = await request(app).put('/api/users/0b091260-10fe-4ac6-b122-e58a398587d9/123')
+        .send(data)
+        expect(response.statusCode).toBe(404)
+        expect(response.body.message).toBe(undefined)
+    });
 })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const assert = require("assert");
-
-// describe("POST /api/users",()=>{
-//   describe("given a name and login",()=>{
-//     //should save the name and login to the database
-//     //should respond with a json object containg whole new user info
-//     test("should respond with a 200 status code", async ()=>{
-//       const response = await request(app).post("/api/users").send({
-//         "name":"name",
-//         "login":"login"
-//       })
-//       expect(response.statusCode).toBe(200)
-//     })
-//     test("should specify json in the content type header",async()=>{
-//       const response = await request(app).post("/api/users").send({
-//         "name":"name",
-//         "login":"login"
-//       })
-//       expect(response.headers['content-type']).toBe(expect.stringContaining("json"))
-//     })
-//   })
-//   describe("when the name and login is missing",()=>{
-//     //should respond with a status code 400
-
-//   })
-//   describe("when the name and login is not a string",()=>{
-
-//   })
-// })
+/*Testing delete cartItem endpoint*/
+describe('DELETE /api/carts/:userLogin/:itemId', function () {
+    it('should respond 200 deleted', function (done) {
+        request(app)
+            .delete('/api/carts/ce6ab499-60fe-43b4-9a1c-e4590edc0540/f7bd1b82-e3cf-4252-b57a-3dbe0dccda13')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .expect((res) => {
+                expect(res.body.message).toEqual('cart item has deleted');
+            })
+            .end((err) => {
+                if (err) return done(err)
+                done();
+            })
+    });
+    it('Testing get a error message by deleting a non-existing user login ', async () => {
+        const response = await request(app).delete('/api/users/123/c845823c-deea-4a8d-bc88-d1d6f48f7561')
+        expect(response.statusCode).toBe(404)
+        expect(response.body.message).toBe(undefined)
+    });
+    it('Testing get a error message by deleting a non-existing item id ', async () => {
+        const response = await request(app).delete('/api/users/0b091260-10fe-4ac6-b122-e58a398587d9/123')
+        expect(response.statusCode).toBe(404)
+        expect(response.body.message).toBe(undefined)
+    });
+})
