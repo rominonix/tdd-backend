c687b8a3c1ccba2d3d5fc549228dcb5cd700a18f
Romina Rebolledo <rominarebolledo@gmail.com>
2021-11-29 09:40:23 +0100

    small fix

---
+++ routes/userRoutes.js
@@ -0,0 +1,12 @@
+const { Router } = require("express");
+
+// req controller
+const userController=require('../controllers/userController')
+const router = Router();
+
+router.get("/users", userController.allUser);
+router.get("/users/:login", userController.getUserByLogin);
+router.post("/users", userController.creatUser);
+router.delete("/users/:login", userController.deleteUser);
+
+module.exports = router;
